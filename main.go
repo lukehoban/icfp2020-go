5bb9d3b7dfd775c5eb266a7bb92380f5e60ce90e
Luke Hoban <lukehoban@microsoft.com>
2025-07-05 10:55:54 -0700

    Working pow2

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ type Expr interface {
 type Ap struct {
 	Left  Expr
 	Right Expr
+
+	// Cached computed value
+	v Expr
 }
 
 type Number struct {
@@ -21,99 +24,6 @@ type Number struct {
 
 type Symbol struct {
 	Name string
-}
-
-type Value interface {
-	Call(arg Value) Value
-}
-
-type Num int64
-
-func (n Num) Call(arg Value) Value {
-	panic(fmt.Sprintf("call: expected function, got number %d", n))
-}
-
-type Fun func(arg Value) Value
-
-func (f Fun) Call(arg Value) Value {
-	return f(arg)
-}
-
-// Assign "nil" after builtins is initialized
-func builtins() map[string]Fun {
-	builtins := map[string]Fun{}
-
-	// 9
-	builtins["mul"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			if n0, ok := x0.(Num); ok {
-				if n1, ok := x1.(Num); ok {
-					return n0 * n1
-				}
-			}
-			panic(fmt.Sprintf("mul: expected two numbers, got %T and %T", x0, x1))
-		})
-	})
-
-	// 11
-	builtins["eq"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			if n0, ok := x0.(Num); ok {
-				if n1, ok := x1.(Num); ok {
-					if n0 == n1 {
-						return builtins["t"]
-					}
-					return builtins["f"]
-				}
-			}
-			panic(fmt.Sprintf("eq: expected two numbers, got %T and %T", x0, x1))
-		})
-	})
-
-	// 18
-	builtins["s"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			return Fun(func(x2 Value) Value {
-				return x0.Call(x2).Call(x1.Call(x2))
-			})
-		})
-	})
-
-	// 19
-	builtins["c"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			return Fun(func(x2 Value) Value {
-				return x0.Call(x2).Call(x1)
-			})
-		})
-	})
-
-	// 20
-	builtins["b"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			return Fun(func(x2 Value) Value {
-				return x0.Call(x1.Call(x2))
-			})
-		})
-	})
-
-	// 21
-	builtins["t"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			return x0
-		})
-	})
-	builtins["cons"] = Fun(func(x0 Value) Value {
-		return Fun(func(x1 Value) Value {
-			return Fun(func(x2 Value) Value {
-				return x2.Call(x0).Call(x1)
-			})
-		})
-	})
-	builtins["nil"] = Fun(func(x0 Value) Value {
-		return builtins["t"]
-	})
-	return builtins
 }
 
 func parseProgram(path string) (map[string]Expr, error) {
@@ -157,30 +67,244 @@ func parseExpr(terms []string) (Expr, []string) {
 	}
 }
 
-func eval(expr Expr, symbols map[string]Fun) (Value, error) {
+func printExpr(expr Expr) string {
 	switch e := expr.(type) {
+	case *Number:
+		return fmt.Sprintf("%d", e.Value)
+	case *Symbol:
+		return e.Name
 	case *Ap:
+		return fmt.Sprintf("ap %s %s", printExpr(e.Left), printExpr(e.Right))
+	default:
+		return fmt.Sprintf("unknown(%T)", expr)
+	}
+}
+
+func eval(expr Expr, symbols map[string]Expr) (Expr, error) {
+	originalExpr := expr
+	for {
+		if a, ok := expr.(*Ap); ok && a.v != nil {
+			return a.v, nil
+		}
+		newExpr, err := evalInner2(expr, symbols)
+		if err != nil {
+			return nil, fmt.Errorf("error evaluating expression: %w", err)
+		}
+		if expr == newExpr {
+			if a, ok := originalExpr.(*Ap); ok {
+				if a.v != nil {
+					panic("shouldn't be here, should have already returned cached value")
+				}
+				fmt.Printf("Caching value for %s: %v\n", printExpr(originalExpr), printExpr(newExpr))
+				a.v = expr
+			}
+			return expr, nil
+		}
+		expr = newExpr
+	}
+}
+
+func evalInner2(expr Expr, symbols map[string]Expr) (Expr, error) {
+	fmt.Printf("Evaluating: %T %v %q\n", expr, expr, printExpr(expr))
+	// Walk down the left spine, pushing arguments to app onto a stack and then applying when we reach
+	// a function.
+	args := []Expr{}
+loop:
+	for {
+		fmt.Printf("Walking down: %T %v %q %v\n", expr, expr, printExpr(expr), args)
+		switch e := expr.(type) {
+		case *Number:
+			if len(args) > 0 {
+				return nil, fmt.Errorf("unexpected number %v with args %v", e.Value, args)
+			}
+			return e, nil
+		case *Symbol:
+			if val, ok := symbols[e.Name]; ok {
+				expr = val
+				continue
+			}
+			switch e.Name {
+			case "add":
+				if len(args) < 2 {
+					return nil, fmt.Errorf("symbol 'add' requires 2 arguments, got %d: %v", len(args), args)
+				}
+				var err error
+				expr, err = twoArgCall(args[0], args[1], symbols, func(left, right *Number) Expr {
+					return &Number{Value: left.Value + right.Value}
+				})
+				if err != nil {
+					return nil, fmt.Errorf("error evaluating 'add': %w", err)
+				}
+				args = args[2:]
+				break loop
+			case "mul":
+				if len(args) < 2 {
+					return nil, fmt.Errorf("symbol 'mul' requires 2 arguments, got %d: %v", len(args), args)
+				}
+				var err error
+				expr, err = twoArgCall(args[0], args[1], symbols, func(left, right *Number) Expr {
+					return &Number{Value: left.Value * right.Value}
+				})
+				if err != nil {
+					return nil, fmt.Errorf("error evaluating 'mul': %w", err)
+				}
+				args = args[2:]
+				break loop
+			case "eq":
+				if len(args) < 2 {
+					return nil, fmt.Errorf("symbol 'eq' requires 2 arguments, got %d: %v", len(args), args)
+				}
+				var err error
+				expr, err = twoArgCall(args[0], args[1], symbols, func(left, right *Number) Expr {
+					if left.Value == right.Value {
+						return &Symbol{Name: "t"}
+					}
+					return &Symbol{Name: "f"}
+				})
+				if err != nil {
+					return nil, fmt.Errorf("error evaluating 'eq': %w", err)
+				}
+				args = args[2:]
+				break loop
+			case "s":
+				// ap ap ap s x0 x1 x2   =   ap ap x0 x2 ap x1 x2
+				if len(args) < 3 {
+					return nil, fmt.Errorf("symbol 's' requires 3 arguments, got %d: %v", len(args), args)
+				}
+				expr = &Ap{Left: &Ap{Left: args[0], Right: args[2]}, Right: &Ap{Left: args[1], Right: args[2]}}
+				args = args[3:]
+				break loop
+			case "c":
+				// ap ap ap c x0 x1 x2   =   ap ap x0 x2 x1
+				if len(args) < 3 {
+					return nil, fmt.Errorf("symbol 'c' requires 3 arguments, got %d: %v", len(args), args)
+				}
+				expr = &Ap{Left: &Ap{Left: args[0], Right: args[2]}, Right: args[1]}
+				args = args[3:]
+				break loop
+			case "b":
+				// ap ap ap b x0 x1 x2   =   ap x0 ap x1 x2
+				if len(args) < 3 {
+					return nil, fmt.Errorf("symbol 'b' requires 3 arguments, got %d: %v", len(args), args)
+				}
+				expr = &Ap{Left: args[0], Right: &Ap{Left: args[1], Right: args[2]}}
+				args = args[3:]
+				break loop
+			case "t":
+				// ap ap t x0 x1   =   x0
+				if len(args) < 2 {
+					return nil, fmt.Errorf("symbol 't' requires 2 arguments, got %d: %v", len(args), args)
+				}
+				expr = args[0]
+				args = args[2:]
+				break loop
+			case "f":
+				// ap ap f x0 x1   =   x1
+				if len(args) < 2 {
+					return nil, fmt.Errorf("symbol 'f' requires 2 arguments, got %d: %v", len(args), args)
+				}
+				expr = args[1]
+				args = args[2:]
+				break loop
+			default:
+				return nil, fmt.Errorf("not yet implemented symbol: %s", e.Name)
+			}
+		case *Ap:
+			// Note: don't eval the right side yet, just push it onto the stack
+			args = append([]Expr{e.Right}, args...)
+			expr = e.Left
+		default:
+			return nil, fmt.Errorf("unknown expression type: %T", expr)
+		}
+	}
+	// Re-apply any remaining arguments
+	for _, arg := range args {
+		expr = &Ap{Left: expr, Right: arg}
+	}
+	return expr, nil
+}
+
+func twoArgCall(x0, x1 Expr, symbols map[string]Expr, f func(left, right *Number) Expr) (Expr, error) {
+	v0, err := eval(x0, symbols)
+	if err != nil {
+		return nil, fmt.Errorf("error evaluating first argument of 'mul': %w", err)
+	}
+	v1, err := eval(x1, symbols)
+	if err != nil {
+		return nil, fmt.Errorf("error evaluating second argument of 'mul': %w", err)
+	}
+	if n1, ok := v0.(*Number); ok {
+		if n2, ok := v1.(*Number); ok {
+			return f(n1, n2), nil
+		} else {
+			return nil, fmt.Errorf("expected number for second argument of 'mul', got: %v", v1)
+		}
+	} else {
+		return nil, fmt.Errorf("expected number for first argument of 'mul', got: %v", v0)
+	}
+}
+
+func evalInner(expr Expr, symbols map[string]Expr) (Expr, error) {
+	fmt.Printf("Evaluating: %T %v\n", expr, expr)
+	switch e := expr.(type) {
+	case *Number:
+		return e, nil
+	case *Symbol:
+		if val, ok := symbols[e.Name]; ok {
+			return val, nil
+		}
+		return e, nil
+	case *Ap:
+		if e.v != nil {
+			return e.v, nil
+		}
 		left, err := eval(e.Left, symbols)
 		if err != nil {
-			return nil, fmt.Errorf("error evaluating left expression: %w", err)
+			return nil, fmt.Errorf("error evaluating left side of application: %w", err)
 		}
-		right, err := eval(e.Right, symbols)
-		if err != nil {
-			return nil, fmt.Errorf("error evaluating right expression: %w", err)
+		switch l := left.(type) {
+		case *Number:
+			return nil, fmt.Errorf("left side of application is a number, expected a function: %v", l)
+		case *Symbol:
+			return nil, fmt.Errorf("not yet implemeneted handling of 1 arg symbol: %s", l.Name)
+		case *Ap:
+			switch l2 := l.Left.(type) {
+			case *Number:
+				return nil, fmt.Errorf("not yet implemeneted handling of application with number: %v", l2)
+			case *Symbol:
+				return nil, fmt.Errorf("not yet implemeneted handling of 2 arg symbol: %s", l2.Name)
+			case *Ap:
+				switch l3 := l2.Left.(type) {
+				case *Number:
+					return nil, fmt.Errorf("not yet implemeneted handling of application with number: %v", l3)
+				case *Symbol:
+					x0 := l2.Right
+					x1 := l.Right
+					x2 := e.Right
+					switch l3.Name {
+					case "s":
+						return &Ap{&Ap{x0, x2, nil}, &Ap{x1, x2, nil}, nil}, nil
+					case "k":
+						return nil, fmt.Errorf("not yet implemeneted handling of symbol: %s", l3.Name)
+					case "i":
+						return nil, fmt.Errorf("not yet implemeneted handling of symbol: %s", l3.Name)
+					default:
+						return nil, fmt.Errorf("unknown function: %s", l3.Name)
+					}
+				case *Ap:
+					return nil, fmt.Errorf("unexpected 4 args application: %v", l3)
+				default:
+					return nil, fmt.Errorf("unknown expression type: %T", l3)
+				}
+			default:
+				return nil, fmt.Errorf("unknown expression type: %T", l2)
+			}
+		default:
+			return nil, fmt.Errorf("unknown expression type: %T", l)
 		}
-		if fun, ok := left.(Fun); ok {
-			return fun(right), nil
-		}
-		return nil, fmt.Errorf("left expression is not a function: %T", left)
-	case *Number:
-		return Num(e.Value), nil
-	case *Symbol:
-		if fun, ok := symbols[e.Name]; ok {
-			return fun, nil
-		}
-		return nil, fmt.Errorf("undefined symbol: %s", e.Name)
+
 	default:
-		return nil, fmt.Errorf("unknown expression type: %T", e)
+		return nil, fmt.Errorf("unknown expression type: %T", expr)
 	}
 }
 
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 	"testing"
 
@@ -8,17 +9,57 @@ import (
 )
 
 func TestPowerOf2(t *testing.T) {
-	symbols := builtins()
 
 	pwr2 := "ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1"
 	e, _ := parseExpr(strings.Split(pwr2, " "))
-	pwr2Val, err := eval(e, symbols)
+	symbols := map[string]Expr{
+		"pwr2": e,
+	}
+
+	for _, testCase := range []struct {
+		input    int64
+		expected int64
+	}{
+		// ap pwr2 0   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+		// ap pwr2 0   =   ap ap ap ap c ap eq 0 1 0 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+		// ap pwr2 0   =   ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+		// ap pwr2 0   =   ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+		// ap pwr2 0   =   1
+		{0, 1},
+		// ap pwr2 1   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap ap ap c ap eq 0 1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap ap ap eq 0 1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap f 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap ap b pwr2 ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap pwr2 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap ap ap c ap eq 0 1 ap ap add -1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 ap ap add -1 1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+		// ap pwr2 1   =   ap ap mul 2 1
+		// ap pwr2 1   =   2
+		{1, 2},
+		{3, 8},
+		{4, 16},
+		{5, 32},
+		{8, 256},
+	} {
+		expr, _ := parseExpr([]string{"ap", "pwr2", strconv.FormatInt(testCase.input, 10)})
+		v, err := eval(expr, symbols)
+		assert.NoError(t, err)
+		assert.Equal(t, &Number{testCase.expected}, v)
+	}
+}
+
+func TestGalaxy(t *testing.T) {
+	symbols, err := parseProgram("galaxy.txt")
 	assert.NoError(t, err)
 
-	symbols["pwr2"] = pwr2Val.(Fun)
-
-	expr, _ := parseExpr([]string{"ap", "pwr2", "0"})
+	expr, _ := parseExpr([]string{"ap", "ap", "galaxy", "nil", "ap", "ap", "vec", "0", "0"})
 	v, err := eval(expr, symbols)
 	assert.NoError(t, err)
-	assert.Equal(t, Num(1), v)
+	assert.Equal(t, &Number{0}, v)
+
 }
